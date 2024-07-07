418b3cb115c1a05ccb6aa12c528e4e66bd841471
shammusaleem <shammusaleem85@gmail.com>
2024-07-08 02:15:57 +0530

    countdown

---
+++ callback hell.js
@@ -0,0 +1,78 @@
+function displayMessage(message) {
+    document.getElementById('countdown').innerText = message;
+}
+
+function countdown() {
+    displayMessage("10");
+    setTimeout(() => {
+        displayMessage("9");
+        setTimeout(() => {
+            displayMessage("8");
+            setTimeout(() => {
+                displayMessage("7");
+                setTimeout(() => {
+                    displayMessage("6");
+                    setTimeout(() => {
+                        displayMessage("5");
+                        setTimeout(() => {
+                            displayMessage("4");
+                            setTimeout(() => {
+                                displayMessage("3");
+                                setTimeout(() => {
+                                    displayMessage("2");
+                                    setTimeout(() => {
+                                        displayMessage("1");
+                                        setTimeout(() => {
+                                            displayMessage("Happy Independence Day");
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+countdown();
+function displayMessage(message) {
+    document.getElementById('countdown').innerText = message;
+}
+
+function countdown() {
+    displayMessage("10");
+    setTimeout(() => {
+        displayMessage("9");
+        setTimeout(() => {
+            displayMessage("8");
+            setTimeout(() => {
+                displayMessage("7");
+                setTimeout(() => {
+                    displayMessage("6");
+                    setTimeout(() => {
+                        displayMessage("5");
+                        setTimeout(() => {
+                            displayMessage("4");
+                            setTimeout(() => {
+                                displayMessage("3");
+                                setTimeout(() => {
+                                    displayMessage("2");
+                                    setTimeout(() => {
+                                        displayMessage("1");
+                                        setTimeout(() => {
+                                            displayMessage("Happy Independence Day");
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+countdown();
